6a467091230c88598688dd71b8deb794bddd7a68
SgLy <775150558@qq.com>
2018-01-05 00:20:06 +0800

    improve performance

--- bp/backend/server.py
+++ bp/backend/server.py
@@ -62,6 +62,7 @@ class Handler(BaseHTTPRequestHandler):
     def response_json(self, data):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-Type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode())
 
--- bp/frontend/js/test.js
+++ bp/frontend/js/test.js
@@ -1,8 +1,11 @@
-'use strict'
+'use strict';
 
-const URL = "http://127.0.0.1:10087";
+/* eslint-disable no-console */
+/* global console, atob, DrawingBoard */
 
-var myBoard;
+const URL = 'http://127.0.0.1:10087';
+
+let myBoard;
 
 function dataURItoBytes(dataURI) {
     var binary = atob(dataURI.split(',')[1]);
@@ -13,97 +16,83 @@ function dataURItoBytes(dataURI) {
     return new Uint8Array(array);
 }
 
-$(document).ready(function() {
-  // get selectable models
-  $.ajax({
-    url: URL + "/model_list",
-    type: "GET",
-    dataType: "json",
-    success: function(res) {
-      let modelList = res.models;
-      for (let i in modelList) {
-        let modelName = modelList[i];
-        $("#model_select").append('<option value="' + modelName
-          + '">' + modelName + '</option>')
-      }
-      // initalize materizl_select
-      $('select').material_select();
-    }
-  });
- 
-  // initalize drawing board
-  $("#drawing").height($("#drawing").width());
-  let drawSize = $("#drawing").width() / 14.0
-  myBoard = new DrawingBoard.Board('drawing', {
-    controls: false,
-    webStorage: false,
-    size: drawSize
-  });
-
-  // clear board
-  $("#clear_board").click(function() {
-    myBoard.reset({
-      background: true
+$(() => {
+    // get selectable models
+    $.get(`${URL}/model_list`, (res) => {
+        let modelList = res.models;
+        modelList.forEach(m => {
+            $('#model_select').append(`<option value="${m}">${m}</option>`);
+        });
+        // initalize materizl_select
+        $('select').material_select();
     });
-    // erase answer
-    $("#prediction").hide();
-    $("#prediction p").text("");
 
-    clearSvg();
-  });
+    // initalize drawing board
+    $('#drawing').height($('#drawing').width());
+    let drawSize = $('#drawing').width() / 14.0;
+    myBoard = new DrawingBoard.Board('drawing', {
+        controls: false,
+        webStorage: false,
+        size: drawSize
+    });
 
-  // submit clicked
-  $("#submit_draw").click(function() {
-    $("#preloader_1").show();
-    $("#prediction").show();
-    let model_selected = $('#model_select').find(":selected").text();
-    let image = dataURItoBytes(myBoard.getImg());
-    $.ajax({
-      url: URL + "/eval?model=" + model_selected,
-      type: "POST",
-      data: image,
-      dataType: "json",
-      processData: false,
-      success: function(res) {
-        $("#preloader_1").hide();
-        $("#prediction p").text(res.ans);
-        option.horizontalPadding = $(".col.s12.m6.l8 .card-content").width() * 0.5 / res.layers.length;
-        // layers to drawing data
-        let d_data = {nodes: []};
-        for (let i in res.layers) {
-          let layer = res.layers[i][0];
-          for (let j in layer) {
-            d_data.nodes.push({label: layer[j].toPrecision(2),
-              layer: parseInt(i) + 1})
-            if (j >= 30) {
-              break;
+    // clear board
+    $('#clear_board').click(function() {
+        myBoard.reset({
+            background: true
+        });
+        // erase answer
+        $('#prediction').hide();
+        $('#prediction p').text('');
+
+        clearSvg();
+    });
+
+    // submit clicked
+    $('#submit_draw').click(function() {
+        $('#preloader_1').show();
+        $('#prediction').show();
+        let model_selected = $('#model_select').find(':selected').text();
+        let image = dataURItoBytes(myBoard.getImg());
+        $.post({
+            url: `${URL}/eval?model=${model_selected}`,
+            data: image,
+            dataType: 'json',
+            processData: false,
+            success: (res) => {
+                $('#preloader_1').hide();
+                $('#prediction p').text(res.ans);
+                option.horizontalPadding = $('.col.s12.m6.l8 .card-content').width() * 0.5 / res.layers.length;
+                // layers to drawing data
+                let d_data = {nodes: []};
+                for (let i in res.layers) {
+                    let layer = res.layers[i][0];
+                    for (let j in layer) {
+                        d_data.nodes.push({
+                            label: layer[j].toPrecision(2),
+                            layer: parseInt(i) + 1
+                        });
+                        // if (j >= 60) break;
+                    }
+                }
+                init(d_data);
             }
-          }
-        }
-        init(d_data);
-      }
+        });
     });
-  });
-  
-  // init svg
-  $(option.svg).attr('height', $("#draw_card").height()).
-    attr('width', $("#svg_con").width());
+
+    // init svg
+    $(option.svg).attr({
+        height: $('#draw_card').height(),
+        width: $('#svg_con').width()
+    }).on('mouseenter', 'circle', function() {
+        $(this).attr('r', Math.max(11 * option.ratio, 1.1));
+    }).on('mouseleave', 'circle', function() {
+        $(this).attr('r', Math.max(10 * option.ratio, 1));
+    });
 });
 
 
 // nodes graph related
-let data = {
-    nodes: [
-        { label: 'i0', layer: 1 },
-        { label: 'i1', layer: 1 },
-        { label: 'h0', layer: 2 },
-        { label: 'i2', layer: 1 },
-        { label: 'h1', layer: 2 },
-        { label: 'h2', layer: 2 },
-        { label: 'h3', layer: 2 },
-        { label: 'o0', layer: 3 }
-    ]
-};
 let option = {
     svg: 'svg',
     ratio: 2,
@@ -116,60 +105,31 @@ let option = {
 function clearSvg() {
     $(option.svg).html('');
 }
-function refreshSvg() {
-    $(option.svg).html($(option.svg).html());
-    $(option.svg).on('mouseenter', 'circle', function() {
-        $(this).attr('r', Math.max(11 * option.ratio, 1.1));
-    }).on('mouseleave', 'circle', function() {
-        $(this).attr('r', Math.max(10 * option.ratio, 1));
-    });
-}
 function addPoint(p) {
-    let x = p.x * option.ratio + option.horizontalOffset;
-    let y = p.y * option.ratio + option.verticalOffset;
-    $('<circle></circle>').attr({
-        'point-id': p.id,
-        cx: x,
-        cy: y,
-        r: Math.max(10 * option.ratio, 1)
-    }).data({
-        x: p.x,
-        y: p.y
-    }).appendTo(option.svg);
-    if (option.ratio >= 1) {
-        $(`<text>${p.label}</text>`).attr({
-            x: x,
-            y: y,
-            'font-size': 7 * option.ratio
-        }).appendTo(option.svg);
-    }
+    let r = Math.max(10 * option.ratio, 1);
+    let res = `<circle cx=${p.x} cy=${p.y} r=${r}></circle>`;
+    if (option.ratio >= 1)
+        res += `<text x=${p.x} y=${p.y} font-size=${7 * option.ratio}>${p.label}</text>`;
+    return res;
 }
-function addPath(p) {
-    let d = {
-        x0: p.start.x * option.ratio + option.horizontalOffset,
-        y0: p.start.y * option.ratio + option.verticalOffset,
-        x1: p.end.x * option.ratio + option.horizontalOffset,
-        y1: p.end.y * option.ratio + option.verticalOffset,
-    };
-    $(`<path d="M${d.x0} ${d.y0} L${d.x1} ${d.y1} Z"></path>`).css({
-        fillopacity: 0,
-        stroke: 'dodgerblue'
-    }).prependTo(option.svg);
-}
-function draw(ps) {
-    clearSvg();
-    ps.forEach(addPoint);
-    ps.forEach(p0 => {
-        ps.forEach(p1 => {
-            if (Math.abs(p0.x - p1.x) !== option.horizontalPadding)
-                return;
-            addPath({
-                start: p0,
-                end: p1
-            });
-        });
+let addPath = (p) => `M${p.start.x} ${p.start.y} L${p.end.x} ${p.end.y} Z`;
+
+function draw(points, path) {
+    points.forEach(p => {
+        p.x = p.absX * option.ratio + option.horizontalOffset;
+        p.y = p.absY * option.ratio + option.verticalOffset;
     });
-    refreshSvg();
+    console.time('drawOnce');
+    console.time('calculatePoints');
+    let point = points.reduce((s, p) => s + addPoint(p), '');
+    console.timeEnd('calculatePoints');
+    console.time('calculatePath');
+    let line = `<path d="${path.map(addPath).join('')}"></path>`;
+    console.timeEnd('calculatePath');
+    console.time('browserDraw');
+    $(option.svg).html(line + point);
+    console.timeEnd('browserDraw');
+    console.timeEnd('drawOnce');
 }
 
 let mouseDown = false;
@@ -183,10 +143,17 @@ function init(data) {
         layerIndex[i] = 0;
     points = data.nodes.map(n => ({
         label: n.label,
-        x: (n.layer - minLayer) * option.horizontalPadding,
-        y: (layerIndex[n.layer]++) * option.verticalPadding
+        absX: (n.layer - minLayer) * option.horizontalPadding,
+        absY: (layerIndex[n.layer]++) * option.verticalPadding
     }));
-    draw(points);
+    let path = points.reduce((arr, p1) =>
+        arr.concat(points.map(p2 =>
+            ({ start: p1, end: p2 })
+        )), []);
+    path = path.filter(p =>
+        Math.abs(p.start.absX - p.end.absX) === option.horizontalPadding
+    );
+    draw(points, path);
 
     $(option.svg).on('mousewheel', (event) => {
         if (!event.altKey)
@@ -194,7 +161,7 @@ function init(data) {
         let r = option.ratio;
         r = Math.max(0.1, Math.min(5, r + event.deltaY * 0.05));
         option.ratio = r;
-        draw(points);
+        draw(points, path);
     }).on('mousemove', (event) => {
         if (!mouseDown)
             return;
@@ -202,8 +169,8 @@ function init(data) {
         option.horizontalOffset += (event.offsetX - x);
         option.verticalOffset += (event.offsetY - y);
         dragOrigin = { x: event.offsetX, y: event.offsetY };
-        draw(points);
-    }).on('mousedown', () => {
+        draw(points, path);
+    }).on('mousedown', (event) => {
         mouseDown = true;
         dragOrigin = { x: event.offsetX, y: event.offsetY };
         $(option.svg).css('cursor', 'move');
@@ -212,5 +179,3 @@ function init(data) {
         $(option.svg).css('cursor', 'default');
     });
 }
-
-    
